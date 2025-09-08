ce495cf63157a0bfb96317e6cbc701d21a012d99
Pieter De Praetere <pieter.de.praetere@helptux.be>
2025-09-08 20:17:59 +0200

    we gaan verder met de nvdg

--- obs-scripts/NachtVanDeGeschiedenis.py
+++ obs-scripts/NachtVanDeGeschiedenis.py
@@ -5,6 +5,7 @@ Dit script wordt vanuit OBS gedraaid.
 """
 import obspython as obs
 from datetime import datetime
+import os
 
 ##
 # Script properties
@@ -13,6 +14,7 @@ def script_description():
     return '''<center><h2>Nacht van de Geschiedenis</h2></center>
     <p>
     Configureer OBS voor de Nacht van de Geschiedenis (CC Nova Wetteren).
+    Bronbestanden moeten geordend zijn per scène.
     </p>
     '''
 
@@ -28,11 +30,42 @@ def script_properties():
     obs.obs_properties_add_text(props, "editie", "Editie", obs.OBS_TEXT_DEFAULT)
     obs.obs_properties_add_int(props, "scenes", "Aantal scènes", 0, 999, 1)
     obs.obs_properties_add_path(props, "bronnen", "Map met alle bronbestanden (afbeeldingen)", obs.OBS_PATH_DIRECTORY, '', None)
+    obs.obs_properties_add_button(props, 'button', 'Aanmaken scenes', script_NachtVanDeGeschiedenis)
     return props
 
 
 def script_update(settings):
-    global editie, scenes, bronnen
-    editie = obs.obs_data_get_string(settings, 'editie')
-    scenes = obs.obs_data_get_int(settings, 'scenes')
-    bronnen = obs.obs_data_get_string(settings, 'bronnen')
+    global EDITIE, SCENES, BRONNEN
+    EDITIE = obs.obs_data_get_string(settings, 'editie')
+    SCENES = obs.obs_data_get_int(settings, 'scenes')
+    BRONNEN = obs.obs_data_get_string(settings, 'bronnen')
+
+def script_unload():
+    pass
+
+
+global ACCEPTED_FILE_EXT
+ACCEPTED_FILE_EXT = ('.bmp', '.tga', '.png', '.jpeg', '.jpg', '.gif')
+
+
+##
+# Main script
+##
+def script_NachtVanDeGeschiedenis():
+    """
+    Maak x aantal scènes aan.
+    Ga naar een map met daarin alle scènes met alle bronbestanden per scène.
+    Maak een bron per scène (scene_$scenenummer_bron_$bronnummer)
+    """
+
+
+def nvdg_add_sources_to_scene(scene_number):
+    """
+    Voeg een set bronnen toe aan een scène.
+    We verwachten die in
+    $bronnen/$scene_number/
+    """
+    scene_path = os.path.join(
+        BRONNEN,
+        '{0}'.format(scene_number)
+    )
